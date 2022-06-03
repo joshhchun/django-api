73a85445807a1c16485372a93dc019f6444cead0
joshhchun <joshhchun@gmail.com>
2022-06-03 13:38:41 -0400

    init

---
+++ django_api/settings/development.py
@@ -0,0 +1,9 @@
+from .base import *
+from .base import env
+
+DEBUG = True
+
+SECRET_KEY = env("DJANGO_SECRET_KEY", 
+                 default="IOPJsdfIOPWIDHJjwipjipdjwijipwjdiWIPDJIPjpwdijijIPDJipjipjdpwiJPIjipWJDIPJPIWDJpWDJIPJiJPWD")
+
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
