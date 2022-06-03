b06fc9f3b4533bf1acf5dae6f23c18d02a45cdf8
joshhchun <joshhchun@gmail.com>
2022-06-03 13:56:08 -0400

    added debug handling loggers

--- django_api/settings/base.py
+++ django_api/settings/base.py
@@ -150,3 +150,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Setting up cross origin API endpoint
 CORS_URLS_REGEX = r"^/api/.*$"
 
+# For logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(name)-12s %(asctime)s %(module)s" "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose"
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
