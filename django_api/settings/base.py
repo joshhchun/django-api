30b1192e655c2e0b7aa03c5d30b87b6079e6a27a
joshhchun <joshhchun@gmail.com>
2022-06-07 16:32:33 -0400

    user auth, login, & pass reset

--- django_api/settings/base.py
+++ django_api/settings/base.py
@@ -33,6 +33,8 @@ THIRD_PARTY_APPS = [
 	"drf_yasg",
 	"corsheaders",
  	"djcelery_email",
+	"djoser",
+ 	"rest_framework_simplejwt",
 ]
 
 LOCAL_APPS = ["core_apps.common", "core_apps.profiles", "core_apps.users"]
@@ -146,7 +148,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Setting up cross origin API endpoint
 CORS_URLS_REGEX = r"^/api/.*$"
-
 AUTH_USER_MODEL="users.User"
 
 
@@ -162,6 +163,44 @@ CELERY_RESULT_SERIALIZER = "json"
 REST_FRAMEWORK = {
 	"EXCEPTION_HANDLER": "core_apps.common.exceptions.common_exception_handler",
 	"NON_FIELD_ERRORS_KEY": "error",
+	"DEFAULT_AUTHENTICATION_CLASSES": (
+		"rest_framework_simplejwt.authentication.JWTAuthentication",
+	)
+}
+
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+	"AUTH_HEADER_TYPES":(
+		"Bearer",
+  		"JWT",
+	),
+	"ACCESS_TOKEN_LIFETIME": timedelta(minutes=720),
+ 	"REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+ 	"SIGNING_KEY": env("SIGNING_KEY"),
+ 	"AUTH_HEADER_NAME": "HTTTP_AUTHORIZATION",
+ 	"AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken")
+}
+
+DJOSER = {
+	"LOGIN_FIELD": "email",
+ 	"USER_CREATE_PASSWORD_RETYPE": True,
+	"USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
+	"PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
+ 	"SEND_CONFIRMATION_EMAIL": True,
+	"PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
+ 	"SET_PASSWORD_RETYPE": True,
+	"PASSWORD_RESET_CONFIRM_RETYPE": True,
+	"USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
+ 	"ACTIVATION_URL": "activate/{uid}/{token}",
+  	"SEND_ACTIVATION_EMAIL": True,
+	"SERIALIZERS": {
+		"user_create": "core_apps.users.serializers.CreateUserSerializer",
+		"user": "core_apps.users.serializers.UserSerializer",
+		"current_user": "core_apps.users.serializers.UserSerializer",
+		"user_delete": "djoser.serializers.UserDeleteSerializer",
+	}
 }
 
 LOGGING = {
